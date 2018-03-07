18174c70eb1c8ce02f3ed2f6ee4a45a87ae8468d
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-07 12:12:05 +0100

    add js function triangleArea

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+var a = 4;
+    h = 6;
+    triangleArea = alert(a*h/2);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
