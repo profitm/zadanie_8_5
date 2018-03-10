f0aae06cbf785b8f948265dba807756f34522a66
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-10 17:06:48 +0100

    update js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // scripts.js
 
-var name = prompt('Enter your name');
+/*var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
-
+*/
 var a = 4;
-    h = 6;
-    triangleArea = alert(a*h/2);
+var h = 6;
+var triangleArea = a * h / 2;
+alert(triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
